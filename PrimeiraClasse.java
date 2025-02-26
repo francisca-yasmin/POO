9d7794d87dbf54abc14645677b195a8d66e2990d
EA - Francisca Yasmin Freitas De Almeida <francisca_yasmin@educadventista.org>
2025-02-25 22:40:48 -0300

    aula teste?

---
+++ PrimeiraClasse.java
@@ -0,0 +1,31 @@
+/* 
+Primeira aula de POO, com conceitos basicos vistos de forma prática.
+Feito por: Fran;
+Em: 25/02
+*/
+
+/*
+	void -> não tem retorno para o usuário;
+		// 5 = 2x
+			metodo1()
+				x = 2 / 5
+				x = 2.5
+				
+				
+		// a = bx
+			metodo2(int a, int b)
+				x = a / b
+				return x
+*/
+
+public class PrimeiraClasse{
+	public static void main(String[]args){
+		System.out.println(" oi mundo");
+		
+	}
+	
+	public float metodo2(int a, int b){
+		float x = a / b;
+		return x;
+	}
+}
