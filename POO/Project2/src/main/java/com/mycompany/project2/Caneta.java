e302549df611c3a2276e14556b195beaba8af532
Francisca Yasmin <yasmimfreitaz5@gmail.com>
2025-03-05 21:40:48 -0300

    Update Caneta.java

--- POO/Project2/src/main/java/com/mycompany/project2/Caneta.java
+++ POO/Project2/src/main/java/com/mycompany/project2/Caneta.java
@@ -9,5 +9,15 @@ package com.mycompany.project2;
  * @author francisca_yasmin
  */
 public class Caneta {
+    public int carga;
+    public boolean tampada;
+    public String cor;
+    
+    public void status(){ //letra minuscula é metodo
+        System.out.println("A cor ad caneta eh" + this.cor);
+        System.out.println("A carga esta em: " + this.carga);
+        System.out.println("A caneta esta: " + this.tampada);
+        
+    }
     
 }
