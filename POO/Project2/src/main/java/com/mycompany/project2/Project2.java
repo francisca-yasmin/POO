c301ce3d6428cbfa1ee241c5418d576eadb6bb03
Francisca Yasmin <yasmimfreitaz5@gmail.com>
2025-03-12 21:27:26 -0300

    Update Project2.java

--- POO/Project2/src/main/java/com/mycompany/project2/Project2.java
+++ POO/Project2/src/main/java/com/mycompany/project2/Project2.java
@@ -5,15 +5,17 @@ public class Project2 {
     public static void main(String[] args) {
        System.out.println("Começando a brincadeira de POO");
 
-        Caneta caneta1 = new Caneta();
+       // Caneta caneta1 = new Caneta();
         //caneta1.cor = "preto";
         //caneta1.tampada = true;
-        caneta1.setCor("azul");
-        caneta1.getCor();
-        caneta1.carga = 100;
+        
+        // caneta1.setCor("azul");
+        // String corAtual = caneta1.getCor();
+        // caneta1.carga = 100;
+        Caneta caneta1 = new Caneta().criarCaneta("vermelha", "fina");
 
-        // Encapsulamento, herança e polimorfismo
         /*
+         Encapsulamento, herança e polimorfismo
         Encapsulamento -> Quando eu restrinjo a minha classe, protegendo os atributos/recursos.
         */
 
