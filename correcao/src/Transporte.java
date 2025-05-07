41bb71c7387fa9124b54207cf6df80a9b654ccab
EA - Francisca Yasmin Freitas De Almeida <francisca_yasmin@educadventista.org>
2025-05-06 22:50:17 -0300

    ??

---
+++ correcao/src/Transporte.java
@@ -0,0 +1,8 @@
+public class Transporte {
+    protected int identificador;
+    private int capacidade;
+
+    public Transporte(int identificador, int capacidade) {
+        this.identificador = identificador;
+    }
+}
